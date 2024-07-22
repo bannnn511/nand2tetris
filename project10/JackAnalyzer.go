b4316d02c246339c1fc1d5c7a2aa92e38bea477c
An Ha <habinhan1999@gmail.com>
2024-07-22 10:49:27 +0700

    refactor readfiles

--- project10/JackAnalyzer.go
+++ project10/JackAnalyzer.go
@@ -13,7 +13,7 @@ func main() {
 		printErr("invalid number of arguments")
 	}
 
-	jackFiles := make([]*os.File, 0)
+	jackFiles := make([]string, 0)
 
 	// Open file
 	file, err := os.OpenFile(os.Args[1], os.O_RDONLY, 0)
@@ -30,27 +30,21 @@ func main() {
 	if fileInfo.IsDir() {
 		jackFiles = getJackFiles(file.Name())
 	} else {
-		jackFiles = append(jackFiles, file)
+		jackFiles = append(jackFiles, file.Name())
 	}
-
-	defer func(files []*os.File) {
-		for _, f := range jackFiles {
-			f.Close()
-		}
-	}(jackFiles)
 
 	var parser Parser
 	for _, jack := range jackFiles {
-		src, err := os.ReadFile(jack.Name())
+		src, err := os.ReadFile(jack)
 		if err != nil {
 			printErr(err.Error())
 		}
-		parser.init(jack.Name(), src)
+		parser.init(jack, src)
 		parser.parseFile()
 		parser.printTree()
 
 		fileXML := parser.GetXML()
-		writeErr := os.WriteFile(jack.Name()+".xml", []byte(fileXML), 0644)
+		writeErr := os.WriteFile(jack+".xml", []byte(fileXML), 0644)
 		if writeErr != nil {
 			printErr(err.Error())
 		}
@@ -63,20 +57,19 @@ func printErr(err string) {
 	os.Exit(1)
 }
 
-func getJackFiles(dir string) []*os.File {
+func getJackFiles(dir string) []string {
 	files, err := os.ReadDir(dir)
-	jackFiles := make([]*os.File, 0, len(files))
+	jackFiles := make([]string, 0, len(files))
 	if err != nil {
 		printErr(err.Error())
 	}
 
 	for _, file := range files {
 		if strings.HasSuffix(file.Name(), ".jack") {
-			f, err := os.OpenFile(dir+"/"+file.Name(), os.O_RDONLY, 0)
 			if err != nil {
 				printErr(err.Error())
 			}
-			jackFiles = append(jackFiles, f)
+			jackFiles = append(jackFiles, file.Name())
 		}
 	}
 
